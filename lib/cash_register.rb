ea74160d2d0dce6350834622594dfaf496a3e3b5
Matt Hough <matt@hough.tv>
2017-10-07 12:01:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :discount, :items :void_last_transaction
+  attr_accessor :total, :discount, :items, :void_last_transaction
 
   def initialize(discount = nil)
     @total = 0
