b1614644b06852ec0c5550f7c30aa0f2831b49e2
Matt Hough <matt@hough.tv>
2017-10-07 11:58:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price * quantity
-    self.items << 
+    self.items.fill(title, array.size, quantity)
   end
 
   def apply_discount
